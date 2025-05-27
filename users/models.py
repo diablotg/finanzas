a916f8cd77cd2d1b52bb444e59f81f9e2354e5c1
Eduardo Torres <fucckthizz@gmail.com>
2025-05-27 14:27:39 -0600

    users/models.py actualizado, se corrige direccion de template de reportes

--- users/models.py
+++ users/models.py
@@ -1,16 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.username
-    
-
-
-
-
-
-
